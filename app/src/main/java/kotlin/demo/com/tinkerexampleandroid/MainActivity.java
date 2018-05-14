2d6e0bc6d699e51a4f4468fc75310b689e4d4b77
skytang <tangqihang163@163.com>
2018-05-14 23:33:31 +0800

    V1.0.1

---
+++ app/src/main/java/kotlin/demo/com/tinkerexampleandroid/MainActivity.java
@@ -0,0 +1,13 @@
+package kotlin.demo.com.tinkerexampleandroid;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
